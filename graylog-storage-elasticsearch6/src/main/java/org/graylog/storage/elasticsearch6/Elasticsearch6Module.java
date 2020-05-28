802f67536488454d9494958fad30c5d528b1786d
Dennis Oelkers <dennis@graylog.com>
2020-05-28 13:18:52 +0200

    Binding implementation for `IndicesAdapter`.

--- graylog-storage-elasticsearch6/src/main/java/org/graylog/storage/elasticsearch6/Elasticsearch6Module.java
+++ graylog-storage-elasticsearch6/src/main/java/org/graylog/storage/elasticsearch6/Elasticsearch6Module.java
@@ -1,9 +1,11 @@
 package org.graylog.storage.elasticsearch6;
 
+import org.graylog2.indexer.indices.IndicesAdapter;
 import org.graylog2.plugin.PluginModule;
 
 public class Elasticsearch6Module extends PluginModule {
     @Override
     protected void configure() {
+        bind(IndicesAdapter.class).to(IndicesAdapterES6.class);
     }
 }
